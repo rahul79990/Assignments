f4a4dd8ae53ebc57eccf6d4006bc64eb820b1a28
rahul79990 <117914520+rahul79990@users.noreply.github.com>
2023-07-09 23:24:57 +0530

    Delete eCommerce/src/main/java/com/Ecommerce/eCommerce/repository directory

--- eCommerce/src/main/java/com/Ecommerce/eCommerce/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Ecommerce.eCommerce.repository;
-
-import com.Ecommerce.eCommerce.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address , Integer> {
-}
--- eCommerce/src/main/java/com/Ecommerce/eCommerce/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Ecommerce.eCommerce.repository;
-
-import com.Ecommerce.eCommerce.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order , Integer> {
-}
